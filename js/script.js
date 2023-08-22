c028362f8daabaff91b9dd146655ea66cee8f7c8
Олександр <moskaletzsanych@gmail.com>
2023-08-22 23:28:21 +0300

    slider-fixed

--- js/script.js
+++ js/script.js
@@ -11,5 +11,25 @@ new Swiper('.testimonial-slider', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+    dynamicBullets: true,
   },
+  grabCursor: true,
+  //   slideToClickedSlide: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewPort: true,
+  },
+  //   slidesPerView: 3, //це для слайдера портфоліо
+  //   watchOverflow: true, //щоб прибрати слайдер, якщо кількість слайдів менша, ніж попереднє налаштування
+  spaceBetween: 50,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 800,
+  //   breakpoints: {
+  //     1200: {
+  //       slidesPerView: 2,
+  //     },
+  //   }, // працює по принципу mobile-first
 });
