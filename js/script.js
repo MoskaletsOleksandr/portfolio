89cd83562926baa53c6208a3e8b465383e3885ad
Олександр <moskaletzsanych@gmail.com>
2023-08-22 21:40:01 +0300

    testimonials-slider-start

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ import { mobMenuOpen, menuContainer, mobMenuClose } from './mob-menu.js';
 import { handleScroll } from './scroll.js';
 
 window.addEventListener('scroll', handleScroll);
+
+new Swiper('.testimonial-slider', {
+  navigation: {
+    nextEl: '.btn-next',
+    prevEl: '.btn-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
