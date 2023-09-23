352413f91a524040740d3cbf1e85b9f946140aa3
MoskaletsOleksandr <moskaletzsanych@gmail.com>
2023-09-23 22:03:12 +0300

    skills-js-works

--- js/skills.js
+++ js/skills.js
@@ -1,42 +1,48 @@
+const stacksSection = document.querySelector('.skills');
 const skillNumbers = document.querySelectorAll('.skill__number');
-const endValues = [85, 65, 75];
-
-skillNumbers.forEach((skillNumber, index) => {
-  let start = null;
-  const endValue = endValues[index];
-  const duration = 2000;
-
-  function animateNumber(timestamp) {
-    if (!start) start = timestamp;
-
-    const progress = timestamp - start;
-    const percentage = Math.min(progress / duration, 1);
-    const currentValue = Math.floor(percentage * endValue);
-
-    skillNumber.innerHTML = currentValue + '%';
-
-    if (percentage < 1) {
-      requestAnimationFrame(animateNumber);
-    }
-  }
-});
-
-const stacksSection = document.querySelector('.stacks');
 const circles = document.querySelectorAll('.circle');
 
-window.addEventListener('scroll', () => {
+const countTo = skillNumber => {
+  const target = skillNumber.dataset.number;
+  const duration = 2000;
+  const start = 0;
+  const interval = 10;
+  const steps = duration / interval;
+  const increment = target / steps;
+
+  let current = start;
+
+  const counter = setInterval(() => {
+    current += increment;
+
+    if (current >= target) {
+      clearInterval(counter);
+      current = target;
+    }
+
+    const roundedValue = Math.floor(current);
+    skillNumber.innerHTML = roundedValue + '%';
+  }, interval);
+};
+
+// circles.forEach(circle => {
+//   const dataNumber = circle.getAttribute('data-number');
+//   circle.style.setProperty('--data-number', dataNumber);
+// });
+
+const handleScroll = () => {
   const sectionPos = stacksSection.getBoundingClientRect().top;
   const screenPos = window.innerHeight;
 
-  if (sectionPos < screenPos) {
-    requestAnimationFrame(animateNumber);
+  if (sectionPos < screenPos / 2) {
+    skillNumbers.forEach(skillNumber => countTo(skillNumber));
     circles.forEach(circle => {
+      const dataNumber = circle.getAttribute('data-number');
+      circle.style.setProperty('--data-number', dataNumber);
       circle.classList.add('animate');
     });
-  } else {
-    start = null;
-    circles.forEach(circle => {
-      circle.classList.remove('animate');
-    });
+    window.removeEventListener('scroll', handleScroll);
   }
-});
+};
+
+window.addEventListener('scroll', handleScroll);
